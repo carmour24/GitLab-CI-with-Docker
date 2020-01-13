a34fb0cf743ed18bcc42c19aeb7ab4764d1e4abc
Chris Armour <chris.armour@gmail.com>
2020-01-13 11:10:17 +0000

    Updates

--- repo/build.gradle.kts
+++ repo/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.testcontainers:testcontainers:1.12.3") 
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
