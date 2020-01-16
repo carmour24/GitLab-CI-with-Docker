15a5849032434ce3bc9add3eb9e7339ced2a54e6
Chris Armour <chris.armour@gmail.com>
2020-01-16 13:31:07 +0000

    Docker in docker is now working. Testcontainers and cypress tests are now added

---
+++ repo/src/test/e2e/cypress/integration/spec.js
@@ -0,0 +1,14 @@
+it('should contain welcome message', () => {
+  cy.visit('/')
+
+  cy.get('body')
+    .should('contain', 'It worked!')
+})
+
+
+  /*it('should fail', () => {
+  cy.visit('/')
+
+  cy.get('body')
+    .should('contain', 'It failed!')
+})*/
